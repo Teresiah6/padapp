687c72ca4c546c3979aafc6cb32cb4435b1589b9
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-11-27 17:26:11 +0300

    add vector drawables and multidex dependencies

--- app/src/main/java/com/example/android/padapp/SplashActivity.kt
+++ app/src/main/java/com/example/android/padapp/SplashActivity.kt
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
 
 
