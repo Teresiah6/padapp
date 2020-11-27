68de7c9f723abcae3418bb18b422e5c3818e46ca
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-11-27 17:54:10 +0300

    file extensions

--- app/src/main/java/com/example/android/padapp/util/extensions.kt
+++ app/src/main/java/com/example/android/padapp/util/extensions.kt
@@ -1,9 +1,16 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.android.padapp.util
 
 import android.content.Context
+import android.graphics.Color
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
+import android.widget.Toast
+import com.example.android.padapp.R
+import com.google.android.material.snackbar.Snackbar
 
 fun animate(context: Context, resId: Int): LayoutAnimationController {
     return AnimationUtils.loadLayoutAnimation(context, resId)
@@ -13,3 +20,24 @@ fun loadAnimation(context: Context, resId: Int): Animation {
     return AnimationUtils.loadAnimation(context, resId)
 }
 
+/**
+* Toast message
+* */
+fun Context.toast(message: String) {
+    Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).apply {
+        show()
+    }
+}
+
+/**
+ * SnackBar
+ * */
+fun View.snackbar(msg: String) {
+    Snackbar.make(this, msg, Snackbar.LENGTH_LONG).also { snackbar ->
+        snackbar.setActionTextColor(Color.WHITE)
+        snackbar.setBackgroundTint(resources.getColor(R.color.colorPrimary))
+        snackbar.setAction("okay") {
+            snackbar.dismiss()
+        }
+    }.show()
+}
