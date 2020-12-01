0105d898d053719038a8243ba64ea957c18d0dc7
Teresiah6 <wangariteresiah6@gmail.com>
2020-12-01 15:08:18 +0300

    nav_graph

--- app/src/main/java/com/example/android/padapp/ui/activities/DonateActivity.kt
+++ app/src/main/java/com/example/android/padapp/ui/activities/DonateActivity.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android.padapp.R
 
-class SelectionActivity : AppCompatActivity() {
-
-
+class DonateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selection)
+        setContentView(R.layout.activity_donate)
+
+
     }
 }
--- app/src/main/java/com/example/android/padapp/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/android/padapp/ui/activities/LoginActivity.kt
@@ -1,12 +1,26 @@
 package com.example.android.padapp.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import com.example.android.padapp.R
+import com.example.android.padapp.databinding.ActivityLoginBinding
+import com.example.android.padapp.databinding.ActivitySplashBinding
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+
+        startActivity(Intent(this@LoginActivity, SignupActivity::class.java))
+        finish()
+
+
     }
 }
